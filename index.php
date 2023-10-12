0eced5aba5ecd052c28b84cdaa1403896d78efa0
Tamim Rahman <tamim101rahman@gmail.com>
2023-10-12 12:32:32 +0600

    Showing Book List in  a Table

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book List</title>
+</head>
+<body>
+    <h1>Book List</h1>
+
+    <?php
+    $bookData = json_decode(file_get_contents('book.json'), true);
+    if ($bookData) {
+        echo '<table border="1">';
+        echo '<tr><th>Title</th><th>Author</th><th>Available</th><th>Pages</th><th>ISBN</th></tr>';
+        foreach ($bookData as $book) {
+            echo '<tr>';
+            echo '<td>' . $book['title'] . '</td>';
+            echo '<td>' . $book['author'] . '</td>';
+            echo '<td>' . ($book['available'] ? 'Yes' : 'No') . '</td>';
+            echo '<td>' . $book['pages'] . '</td>';
+            echo '<td>' . $book['isbn'] . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        echo 'No books found.';
+    }
+    ?>
+</body>
+</html>
